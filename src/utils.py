37ef607b8658b1ad8a007151ca7692ea25fb5ae8
sheikhishmam2002 <latifurahman2@gmail.com>
2025-11-28 18:36:47 +0600

    Prediction Pipeline

--- src/utils.py
+++ src/utils.py
@@ -5,6 +5,7 @@ import dill ## helps to create the pickle file
 import pandas as pd
 import numpy as np
 from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV
 
 from src.exception import CustomException
 
@@ -19,12 +20,17 @@ def save_obj(file_path,obj):
     except Exception as e:
         raise CustomException(e,sys)
     
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train,y_train,X_test,y_test,models,param):
     try:
         report = {}
         for i in range(len(list(models))):
             model = list(models.values())[i]
-                
+            para=param[list(models.keys())[i]]
+            
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train,y_train)
+            
+            model.set_params(**gs.best_params_)
             model.fit(X_train,y_train)
                 
             y_train_pred=model.predict(X_train)
@@ -36,5 +42,12 @@ def evaluate_models(X_train,y_train,X_test,y_test,models):
             report[list(models.keys())[i]] = test_model_score
                 
         return report
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+def load_object(file_path):
+    try:
+        with open(file_path,'rb') as file_obj:
+            return dill.load(file_obj)
     except Exception as e:
         raise CustomException(e,sys)
